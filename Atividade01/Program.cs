4f552709daa3ed44445cd7d73a4fbe5e431d1268
Paulorrss <91346980+Paulorrss@users.noreply.github.com>
2022-06-19 18:29:01 -0400

    Update Program.cs

--- Atividade01/Program.cs
+++ Atividade01/Program.cs
@@ -1,13 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Atividade01
+namespace ATIVIDADE01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int valor = 0, contador = 0, numero;
+
+            Console.WriteLine("Favor usuário, digitar quantidade de números: ");
+            valor = int.Parse(Console.ReadLine());
+
+            for (int i = 0; valor > i; i++)
+            {
+                Console.WriteLine("Favor digitar valor do número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero > 100)
+                {
+                    contador++;
+                }
+            }
+
+            Console.WriteLine("Quantidade de números maiores que cem são: " + contador);
+
+            Console.ReadKey();
         }
     }
 }
+
+
